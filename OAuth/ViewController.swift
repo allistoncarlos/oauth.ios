0cfc992e1503668a9360847d2c4982e08b3bb62a
Alliston Carlos Rosa Aleixo <alliston@outlook.com>
2018-04-07 12:03:52 -0300

    Criação do projeto

---
+++ OAuth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OAuth
+//
+//  Created by Alliston Aleixo on 07/04/2018.
+//  Copyright © 2018 Alliston Aleixo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
